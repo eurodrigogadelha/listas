429f0a9565281470a65c8ccee278dc41bc1943bc
igorgadelhabl <IGORGADELHABL@gmail.com>
2023-06-02 23:08:47 -0300

    first commit

---
+++ LISTAS-WULDSON/TUPLAS/ex002.py
@@ -0,0 +1,12 @@
+# Questão 2 - Dicionário de números favoritos
+
+numeros_favoritos = {
+    "Kaio": 22,
+    "Jehran": 17,
+    "Augusto": 69,
+    "Rodrigo": 24,
+    "Iuri": 9
+}
+
+for pessoa, numero in numeros_favoritos.items():
+    print(pessoa + ":", numero)
